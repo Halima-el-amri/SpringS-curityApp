c32723397643e1b40f9c64329ab4bff3f8a0609b
EL AMRI HALIMA <elamrihalima1@gmail.com>
2024-02-20 15:27:44 +0100

    Dockirization de projet

--- src/main/java/org/apache/maven/archetypes/labxpertproject/config/SwaggerConfig.java
+++ src/main/java/org/apache/maven/archetypes/labxpertproject/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("org.apache.maven.archetypes.labxpertproject.controller"))
+                .apis(RequestHandlerSelectors.basePackage("org.apache.maven.archetypes.labxpertproject"))
                 .paths(PathSelectors.any())
                 .build();
     }
